ff9936550a39321c5c7ec782f2d0e5da3b08b688
Jack625a <77822933+Jack625a@users.noreply.github.com>
2024-05-02 19:30:37 -0400

    Primer Parcial - Pautas

---
+++ examen.py
@@ -0,0 +1,48 @@
+class MaterialBiblioteca:
+    def __init__(self, atributos):
+
+
+    #Acciones
+    def tomar_prestado(self):
+        if #Prestamo por disponibilidad
+    
+    def devolver(self):
+
+#subclase
+class Libro(MaterialBiblioteca):
+    #atributos heredados
+    #atributos propios (ej. editorial, edicion)
+
+class Revista(MaterialBiblioteca):
+    #atributos heredados
+    #atributos propios (ej. editorial, edicion)
+
+class Periodico(MaterialBiblioteca):
+    #atributos heredados
+    #atributos propios (ej. editorial, edicion)
+
+class UsuarioBiblioteca:
+
+    #Metodo para agregar nuevo usuario
+    self.usuarios=[]
+    def agregra_usuario(self,usuario)
+    usuarios.append(usuario)
+
+#subclases de la clase padre UsuarioBiblioteca
+class Estudiante(UsuarioBiblioteca):
+
+class Profesor(UsuarioBiblioteca):
+
+class Visitante(UsuarioBiblioteca):
+
+
+#metodo para agtre    
+
+
+
+
+#obejtos
+libro1=Libro()
+revista1=Revista()
+periodico1=Periodico()
+
